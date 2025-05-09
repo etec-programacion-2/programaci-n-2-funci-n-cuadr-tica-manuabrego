30828ab6ed3ea13da1d2b75ff824c3e0e23ccad8
manuabrego <ms.abrego@alumno.etec.um.edu.ar>
2025-05-09 10:22:41 -0300

    feat: Estructura básica y definición de coeficientes

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,17 @@
+package org.example
+fun main() {
+    val a = 3
+    val b = 5
+    val c = 9
+
+    if (a == 0) {
+        println("a no puede ser 0")
+        return
+    } else {
+        println("Ecuación cuadrática: ${a}x² + ${b}x + $c = 0")
+    
+}
+
+
+
+}

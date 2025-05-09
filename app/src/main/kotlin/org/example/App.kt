4738fc09926755ae61d31568562431c3ee0b78c1
manuabrego <ms.abrego@alumno.etec.um.edu.ar>
2025-05-09 10:34:38 -0300

    feat: Cálculo de valores de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -8,10 +8,20 @@ fun main() {
         println("a no puede ser 0")
         return
     } else {
-        println("Ecuación cuadrática: ${a}x² + ${b}x + $c = 0")
+        println("Ecuación cuadrática: ${a}x² + ${b}x + $c = 0")}
     
-}
+        
+        
+    }
+fun cuadrática(a: Int, b: Int, c: Int){
+    println("Tabla de valores x, f(x) :")
+    println("------------------------")
+    println("x \t| f(x)")
+    println("--------------")
 
-
-
-}
+    
+    for (x in -5..5) {
+        val fx = a * x * x + b * x + c  
+        println("$x \t| $fx")
+    }
+} 
